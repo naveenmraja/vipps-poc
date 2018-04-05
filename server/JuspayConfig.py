3910377d5faf3ccf9c6c265891b28a5282b76e86
Naveen Raja <naveen.raja@juspay.in>
2018-04-05 12:50:02 +0530

    return url change

--- server/JuspayConfig.py
+++ server/JuspayConfig.py
@@ -3,4 +3,4 @@
 merchant_id = 'vipps_poc'
 environment = 'sandbox'
 api_key = '65E89A62268C4147A22C1810FC01193B'
-return_url = 'http://localhost:5000/orders/:orderId/receipt'
+return_url = 'https://vippspoc.herokuapp.com/orders/:orderId/receipt'
